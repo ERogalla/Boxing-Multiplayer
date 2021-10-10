7f00524537cb98b795142f6da5b86594b393e4d6
ERogalla <erogalla@utexas.edu>
2021-10-09 23:24:50 -0500

    loaded player

--- game.py
+++ game.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 import pygame
 
+from pygame.image import load
+from pygame.transform import scale
+
 pygame.init()
 
 screen = pygame.display.set_mode((675, 450))
 
-background = pygame.image.load('assets/background.png')
+background = load('assets/background.png')
+
+player1Img = scale(load('assets/player1.png'), (30, 111))
+player1x = 150
 
 running = True
 while running:
+    # Background
     screen.blit(background, (0, 0))
+
+    # Player 1
+    screen.blit(player1Img, (player1x, 200))
 
     # Handle Events
     for event in pygame.event.get():
