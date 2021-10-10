e7a947eef94eae755f45f82abf99a98abb8c5144
ERogalla <erogalla@utexas.edu>
2021-10-09 23:17:25 -0500

    close events

--- game.py
+++ game.py
@@ -5,10 +5,18 @@ pygame.init()
 
 screen = pygame.display.set_mode((675, 450))
 
-background = pygame.image.load('assets/background.png').convert()
+background = pygame.image.load('assets/background.png')
 
 running = True
 while running:
-    # Background
     screen.blit(background, (0, 0))
+
+    # Handle Events
+    for event in pygame.event.get():
+        # check if the event is the X button
+        if event.type==pygame.QUIT:
+            # if it is quit the game
+            pygame.quit()
+            exit(0)
+
     pygame.display.update()
